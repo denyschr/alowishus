45bf77b23b265bc35832e3138c313414cc489c41
denyschr <denprogman99@gmail.com>
2025-06-30 21:24:24 +0300

    refactor: replace resize event listener with media query list

--- scripts/header.js
+++ scripts/header.js
@@ -1,9 +1,10 @@
-import { debounce, throttle } from "./utils.js";
+import { throttle } from "./utils.js";
 
 const initHeader = () => {
   const root = document.documentElement;
   const header = document.querySelector("[data-header]");
   const toggleMenuButton = document.querySelector("[data-toggle-menu-button]");
+  const mediaQuery = window.matchMedia("(max-width: 61.99875em)");
 
   const toggleMenu = () => {
     root.classList.toggle("prevent-scroll");
@@ -13,13 +14,13 @@ const initHeader = () => {
     root.classList.toggle("menu-open");
   };
 
-  const closeMenu = debounce(() => {
-    if (window.innerWidth >= 992) {
+  const updateMenu = (mq) => {
+    if (!mq.matches) {
       root.classList.remove("prevent-scroll");
       root.classList.remove("menu-open");
       toggleMenuButton.setAttribute("aria-expanded", "false");
     }
-  });
+  };
 
   const updateHeaderStyle = throttle(() => {
     window.scrollY > 0
@@ -27,10 +28,11 @@ const initHeader = () => {
       : header.classList.remove("scroll");
   });
 
+  updateMenu(mediaQuery);
   updateHeaderStyle();
   toggleMenuButton.addEventListener("click", toggleMenu);
+  mediaQuery.addEventListener("change", updateMenu);
   window.addEventListener("scroll", updateHeaderStyle);
-  window.addEventListener("resize", closeMenu);
 };
 
 export default initHeader;
